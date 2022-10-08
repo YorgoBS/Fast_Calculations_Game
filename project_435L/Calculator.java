be9446a9a6610748a78caad27d407d66b250a951
Yorgo Bou Samra <71931530+YorgoBS@users.noreply.github.com>
2022-10-09 01:44:43 +0300

    Including Level 3 Functionality

--- project_435L/Calculator.java
+++ project_435L/Calculator.java
@@ -12,7 +12,7 @@ public class Calculator {
 	private int op;
 	private List<Integer> result;
 	private List<String> expressions;
-
+	
 	
 	public Calculator(int level) {
 		this.level = level;
@@ -20,7 +20,7 @@ public class Calculator {
 		this.expressions = new ArrayList<String>();
 }
 
-	public void GenerateRandomNumbers() {
+	public void GenerateRandomNumbers(int divisor) {
 		
 		Random rdInt = new Random();
 		if(this.level == 1) {
@@ -32,45 +32,138 @@ public class Calculator {
 			this.secondNumber = rdInt.nextInt(100);
 		}
 		else {
-			this.firstNumber = rdInt.nextInt(100);
-			this.secondNumber = rdInt.nextInt(100);			
-			this.thirdNumber = rdInt.nextInt(100);	
+			switch(divisor) {
+			case 0:
+				this.firstNumber = rdInt.nextInt(100);
+				this.secondNumber = rdInt.nextInt(100);			
+				this.thirdNumber = rdInt.nextInt(100);
+			case 1:
+				this.firstNumber = rdInt.nextInt(50)*2;
+				this.secondNumber = rdInt.nextInt(50)*2;
+				if(this.secondNumber ==0) {
+					this.secondNumber = rdInt.nextInt(50)*2;
+				}
+				this.thirdNumber = rdInt.nextInt(100);
+			case 2:
+				this.firstNumber = rdInt.nextInt(100);
+				this.secondNumber = rdInt.nextInt(50)*2;			
+				this.thirdNumber = rdInt.nextInt(50)*2;
+				if(this.thirdNumber ==0) {
+					this.thirdNumber = rdInt.nextInt(50)*2;
+				}
+			case 3:
+				this.firstNumber = rdInt.nextInt(50)*2;
+				this.secondNumber = rdInt.nextInt(50)*2;
+				if(this.secondNumber ==0) {
+					this.secondNumber = rdInt.nextInt(50)*2;
+				}
+				this.thirdNumber = rdInt.nextInt(50)*2;
+				if(this.thirdNumber ==0) {
+					this.thirdNumber = rdInt.nextInt(50)*2;
+				}
+			}
 		}
 	}
 	
 	public List<String> GenerateRandomExpression() {
 		Random rdOp = new Random();
+		int a;
+		int b;
+		int c;
 		List<String> expResult = new ArrayList<String>();
 		if(this.level < 3) {
 			this.op = rdOp.nextInt(2);
+			GenerateRandomNumbers(0);
+			a = this.firstNumber;
+			b = this.secondNumber;
+			c = this.thirdNumber;
 			switch(this.op) {
 			case 0:
-				expResult.add(Integer.toString(this.firstNumber + this.secondNumber));
-				expResult.add(Integer.toString(this.firstNumber) + "+" + Integer.toString(this.secondNumber));
+				expResult.add(Integer.toString(a + b));
+				expResult.add(Integer.toString(a) + "+" + Integer.toString(b));
 			case 1:
-				expResult.add(Integer.toString(this.firstNumber - this.secondNumber));
-				expResult.add(Integer.toString(this.firstNumber) + "-" + Integer.toString(this.secondNumber));
+				expResult.add(Integer.toString(a - b));
+				expResult.add(Integer.toString(a) + "-" + Integer.toString(b));
+			}
 		}
-//		else {
-//			this.op = rdOp.nextInt(4);
-//			switch(this.op) {
-//			case 0:
-//				this.result = this.firstNumber + this.secondNumber + this.thirdNumber;
-//				return Integer.toString(this.firstNumber) + "+" + Integer.toString(this.secondNumber);
-//			case 1:
-//				this.result = this.firstNumber - this.secondNumber + this.thirdNumber;
-//				return Integer.toString(this.firstNumber) + "-" + Integer.toString(this.secondNumber);
-//			case 2:
-//				this.result = this.firstNumber * this.secondNumber + this.thirdNumber;
-//				return Integer.toString(this.firstNumber) + "*" + Integer.toString(this.secondNumber);
-//			case 3:
-//				this.result = this.firstNumber / this.secondNumber + this.thirdNumber;
-//				return Integer.toString(this.firstNumber) + "*" + Integer.toString(this.secondNumber) ;
-//			}
-//		}
+		else {
+			this.op = rdOp.nextInt(16);
+			if(this.op == 10 || this.op == 12 || this.op == 14) {
+				GenerateRandomNumbers(1);
+
+			}
+			if(this.op == 9 || this.op == 11 || this.op == 13) {
+				GenerateRandomNumbers(2);
+
+			}
+			if(this.op == 15) {
+				GenerateRandomNumbers(3);
+			}
+			else {
+				GenerateRandomNumbers(0);
+			}
+			a = this.firstNumber;
+			b = this.secondNumber;
+			c = this.thirdNumber;
+			switch(this.op) {
+			case 0:
+				expResult.add(Integer.toString(a + b + c));
+				expResult.add(Integer.toString(a) + "+" + Integer.toString(b) + "+" +  Integer.toString(c));
+			case 1:
+				expResult.add(Integer.toString(a + b - c));
+				expResult.add(Integer.toString(a) + "+" + Integer.toString(b) + "-" +  Integer.toString(c));
+			case 2:
+				expResult.add(Integer.toString(a - b + c));
+				expResult.add(Integer.toString(a) + "-" + Integer.toString(b) + "+" +  Integer.toString(c));
+			case 3:
+				expResult.add(Integer.toString(a - b - c));
+				expResult.add(Integer.toString(a) + "-" + Integer.toString(b) + "-" +  Integer.toString(c));
+			
+			case 4:
+				expResult.add(Integer.toString(a + b * c));
+				expResult.add(Integer.toString(a) + "+" + Integer.toString(b) + "*" +  Integer.toString(c));
+			case 5:
+				expResult.add(Integer.toString(a * b + c));
+				expResult.add(Integer.toString(a) + "*" + Integer.toString(b) + "+" +  Integer.toString(c));
+			case 6:
+				expResult.add(Integer.toString(a - b * c));
+				expResult.add(Integer.toString(a) + "-" + Integer.toString(b) + "*" +  Integer.toString(c));
+			case 7:
+				expResult.add(Integer.toString(a * b - c));
+				expResult.add(Integer.toString(a) + "*" + Integer.toString(b) + "-" +  Integer.toString(c));
+			
+			case 8:
+				expResult.add(Integer.toString(a * b * c));
+				expResult.add(Integer.toString(a) + "*" + Integer.toString(b) + "*" +  Integer.toString(c));
+			case 9:
+				expResult.add(Integer.toString(a + b / c));
+				expResult.add(Integer.toString(a) + "+" + Integer.toString(b) + "/" +  Integer.toString(c));
+			case 10:
+				expResult.add(Integer.toString(a / b + c));
+				expResult.add(Integer.toString(a) + "/" + Integer.toString(b) + "+" +  Integer.toString(c));
+			case 11:
+				expResult.add(Integer.toString(a - b / c));
+				expResult.add(Integer.toString(a) + "-" + Integer.toString(b) + "/" +  Integer.toString(c));
+			
+			case 12:
+				expResult.add(Integer.toString(a / b - c));
+				expResult.add(Integer.toString(a) + "/" + Integer.toString(b) + "-" +  Integer.toString(c));
+			case 13:
+				expResult.add(Integer.toString(a * b / c));
+				expResult.add(Integer.toString(a) + "*" + Integer.toString(b) + "/" +  Integer.toString(c));
+			case 14:
+				expResult.add(Integer.toString(a / b * c));
+				expResult.add(Integer.toString(a) + "/" + Integer.toString(b) + "*" +  Integer.toString(c));
+			case 15:
+				expResult.add(Integer.toString(a / b / c));
+				expResult.add(Integer.toString(a) + "/" + Integer.toString(b) + "/" +  Integer.toString(c));
+			}
+			
+			
 		}
 		return expResult;
 	}
+	
 	public List<Integer> getResult() {
 		return result;
 	}
@@ -82,8 +175,9 @@ public class Calculator {
 
 
 	public void GenerateExpressionList(int n) {
+		
 		for(int i = 0;i<n;i++) {
-			GenerateRandomNumbers();
+			
 			List<String> exp = GenerateRandomExpression();
 			this.expressions.add(exp.get(1));
 			this.result.add(Integer.valueOf(exp.get(0)));
--- project_435L/Main.java
+++ project_435L/Main.java
@@ -5,11 +5,11 @@ import java.util.List;
 public class Main {
 
 	public static void main(String[] args) {
-	Calculator calc = new Calculator(1);
-	calc.GenerateExpressionList(10);
+	Calculator calc = new Calculator(3);
+	calc.GenerateExpressionList(40);
 	List<String> expressions = calc.getExpressions();
 	List<Integer> results = calc.getResult();
-	for(int i = 0;i<10;i++) {
+	for(int i = 0;i<40;i++) {
 		System.out.println(expressions.get(i));
 		System.out.println(results.get(i));
 	}
