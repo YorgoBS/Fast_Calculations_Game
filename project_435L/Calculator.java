4947020fc643f9c9cad6f1fabb345fe0cf41a230
Yorgo Bou Samra <71931530+YorgoBS@users.noreply.github.com>
2022-10-09 15:39:35 +0300

    Bug Fix for Level 3

--- project_435L/Calculator.java
+++ project_435L/Calculator.java
@@ -1,9 +1,9 @@
 package project_435L;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Random;
-
 public class Calculator {
 	private int level;
 	private int firstNumber;
@@ -12,12 +12,14 @@ public class Calculator {
 	private int op;
 	private List<Integer> result;
 	private List<String> expressions;
+	private HashMap<String,Integer> expMap;
 	
 	
 	public Calculator(int level) {
 		this.level = level;
 		this.result = new ArrayList<Integer>();
 		this.expressions = new ArrayList<String>();
+		this.expMap = new HashMap<String,Integer>();
 }
 
 	public void GenerateRandomNumbers(int divisor) {
@@ -32,35 +34,62 @@ public class Calculator {
 			this.secondNumber = rdInt.nextInt(100);
 		}
 		else {
+			
 			switch(divisor) {
 			case 0:
 				this.firstNumber = rdInt.nextInt(100);
 				this.secondNumber = rdInt.nextInt(100);			
 				this.thirdNumber = rdInt.nextInt(100);
+				break;
 			case 1:
+				
 				this.firstNumber = rdInt.nextInt(50)*2;
 				this.secondNumber = rdInt.nextInt(50)*2;
-				if(this.secondNumber ==0) {
+				this.thirdNumber = rdInt.nextInt(100);
+
+				while(this.secondNumber ==0){
 					this.secondNumber = rdInt.nextInt(50)*2;
 				}
-				this.thirdNumber = rdInt.nextInt(100);
+				if(this.secondNumber>this.firstNumber && this.firstNumber != 0) {
+					
+					int temp = this.firstNumber;
+					this.firstNumber = this.secondNumber;
+					this.secondNumber = temp;
+				}
+				break;
 			case 2:
 				this.firstNumber = rdInt.nextInt(100);
 				this.secondNumber = rdInt.nextInt(50)*2;			
 				this.thirdNumber = rdInt.nextInt(50)*2;
-				if(this.thirdNumber ==0) {
+				while(this.thirdNumber == 0) {
 					this.thirdNumber = rdInt.nextInt(50)*2;
 				}
-			case 3:
-				this.firstNumber = rdInt.nextInt(50)*2;
-				this.secondNumber = rdInt.nextInt(50)*2;
-				if(this.secondNumber ==0) {
-					this.secondNumber = rdInt.nextInt(50)*2;
+				if(this.thirdNumber>this.secondNumber && this.secondNumber != 0) {
+					int temp = this.secondNumber;
+					this.secondNumber = this.thirdNumber;
+					this.thirdNumber = temp;
 				}
-				this.thirdNumber = rdInt.nextInt(50)*2;
-				if(this.thirdNumber ==0) {
-					this.thirdNumber = rdInt.nextInt(50)*2;
-				}
+				break;
+//			case 3:
+//				this.firstNumber = rdInt.nextInt(50)*2;
+//				this.secondNumber = rdInt.nextInt(50)*2;
+//				if(this.secondNumber ==0) {
+//					this.secondNumber = rdInt.nextInt(50)*2;
+//				}
+//				this.thirdNumber = rdInt.nextInt(50)*2;
+//				if(this.thirdNumber ==0) {
+//					this.thirdNumber = rdInt.nextInt(50)*2;
+//				}
+//				if(this.secondNumber>this.firstNumber && this.firstNumber != 0) {
+//					int temp = this.firstNumber;
+//					this.firstNumber = this.secondNumber;
+//					this.secondNumber = temp;
+//				}
+//				if(this.thirdNumber>this.secondNumber && this.secondNumber != 0) {
+//					int temp = this.secondNumber;
+//					this.secondNumber = this.thirdNumber;
+//					this.thirdNumber = temp;
+//				}
 			}
 		}
 	}
@@ -77,90 +106,123 @@ public class Calculator {
 			a = this.firstNumber;
 			b = this.secondNumber;
 			c = this.thirdNumber;
+			
 			switch(this.op) {
 			case 0:
 				expResult.add(Integer.toString(a + b));
 				expResult.add(Integer.toString(a) + "+" + Integer.toString(b));
+				break;
 			case 1:
 				expResult.add(Integer.toString(a - b));
 				expResult.add(Integer.toString(a) + "-" + Integer.toString(b));
 			}
 		}
 		else {
-			this.op = rdOp.nextInt(16);
+			this.op = rdOp.nextInt(15);
 			if(this.op == 10 || this.op == 12 || this.op == 14) {
 				GenerateRandomNumbers(1);
 
 			}
-			if(this.op == 9 || this.op == 11 || this.op == 13) {
+			else if(this.op == 9 || this.op == 11 || this.op == 13) {
 				GenerateRandomNumbers(2);
 
 			}
-			if(this.op == 15) {
-				GenerateRandomNumbers(3);
-			}
+//			if(this.op == 15) {
+//				GenerateRandomNumbers(3);
+//			}
 			else {
 				GenerateRandomNumbers(0);
+				
 			}
 			a = this.firstNumber;
 			b = this.secondNumber;
 			c = this.thirdNumber;
+			
 			switch(this.op) {
 			case 0:
 				expResult.add(Integer.toString(a + b + c));
 				expResult.add(Integer.toString(a) + "+" + Integer.toString(b) + "+" +  Integer.toString(c));
+				break;
 			case 1:
 				expResult.add(Integer.toString(a + b - c));
 				expResult.add(Integer.toString(a) + "+" + Integer.toString(b) + "-" +  Integer.toString(c));
+				break;
+
 			case 2:
 				expResult.add(Integer.toString(a - b + c));
 				expResult.add(Integer.toString(a) + "-" + Integer.toString(b) + "+" +  Integer.toString(c));
+				break;
+
 			case 3:
 				expResult.add(Integer.toString(a - b - c));
 				expResult.add(Integer.toString(a) + "-" + Integer.toString(b) + "-" +  Integer.toString(c));
+				break;
+
 			
 			case 4:
 				expResult.add(Integer.toString(a + b * c));
 				expResult.add(Integer.toString(a) + "+" + Integer.toString(b) + "*" +  Integer.toString(c));
+				break;
+
 			case 5:
 				expResult.add(Integer.toString(a * b + c));
 				expResult.add(Integer.toString(a) + "*" + Integer.toString(b) + "+" +  Integer.toString(c));
+				break;
+
 			case 6:
 				expResult.add(Integer.toString(a - b * c));
 				expResult.add(Integer.toString(a) + "-" + Integer.toString(b) + "*" +  Integer.toString(c));
+				break;
+
 			case 7:
 				expResult.add(Integer.toString(a * b - c));
 				expResult.add(Integer.toString(a) + "*" + Integer.toString(b) + "-" +  Integer.toString(c));
+				break;
+
 			
 			case 8:
 				expResult.add(Integer.toString(a * b * c));
 				expResult.add(Integer.toString(a) + "*" + Integer.toString(b) + "*" +  Integer.toString(c));
+				break;
+
 			case 9:
 				expResult.add(Integer.toString(a + b / c));
 				expResult.add(Integer.toString(a) + "+" + Integer.toString(b) + "/" +  Integer.toString(c));
+				break;
+
 			case 10:
 				expResult.add(Integer.toString(a / b + c));
 				expResult.add(Integer.toString(a) + "/" + Integer.toString(b) + "+" +  Integer.toString(c));
+				break;
+
 			case 11:
 				expResult.add(Integer.toString(a - b / c));
 				expResult.add(Integer.toString(a) + "-" + Integer.toString(b) + "/" +  Integer.toString(c));
+				break;
+
 			
 			case 12:
 				expResult.add(Integer.toString(a / b - c));
 				expResult.add(Integer.toString(a) + "/" + Integer.toString(b) + "-" +  Integer.toString(c));
+				break;
+
 			case 13:
 				expResult.add(Integer.toString(a * b / c));
 				expResult.add(Integer.toString(a) + "*" + Integer.toString(b) + "/" +  Integer.toString(c));
+				break;
+
 			case 14:
 				expResult.add(Integer.toString(a / b * c));
 				expResult.add(Integer.toString(a) + "/" + Integer.toString(b) + "*" +  Integer.toString(c));
-			case 15:
-				expResult.add(Integer.toString(a / b / c));
-				expResult.add(Integer.toString(a) + "/" + Integer.toString(b) + "/" +  Integer.toString(c));
+				break;
+
+//			case 15:
+//				expResult.add(Integer.toString(a / b / c));
+//				expResult.add(Integer.toString(a) + "/" + Integer.toString(b) + "/" +  Integer.toString(c));
 			}
-			
-			
+					
 		}
+		
 		return expResult;
 	}
 	
@@ -179,10 +241,17 @@ public class Calculator {
 		for(int i = 0;i<n;i++) {
 			
 			List<String> exp = GenerateRandomExpression();
-			this.expressions.add(exp.get(1));
-			this.result.add(Integer.valueOf(exp.get(0)));
+			this.expMap.put(exp.get(1), Integer.valueOf(exp.get(0)));
 			
 		}
+	}
+
+	public HashMap<String, Integer> getExpMap() {
+		return expMap;
+	}
+
+	public void setExpMap(HashMap<String, Integer> expMap) {
+		this.expMap = expMap;
 	}
 	
 }
--- project_435L/Main.java
+++ project_435L/Main.java
@@ -1,18 +1,16 @@
 package project_435L;
 
-import java.util.List;
+import java.util.Map;
 
 public class Main {
 
 	public static void main(String[] args) {
 	Calculator calc = new Calculator(3);
-	calc.GenerateExpressionList(40);
-	List<String> expressions = calc.getExpressions();
-	List<Integer> results = calc.getResult();
-	for(int i = 0;i<40;i++) {
-		System.out.println(expressions.get(i));
-		System.out.println(results.get(i));
-	}
+	calc.GenerateExpressionList(100);
+		Map<String,Integer> testMap = calc.getExpMap();
+		for (Map.Entry<String,Integer> entry : testMap.entrySet())  
+            System.out.println("Key = " + entry.getKey() + 
+                             ", Value = " + entry.getValue()); 
+    } 
+}
 
-}
-}
