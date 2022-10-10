a176763830985f6622bdf8d87439f90fac7c61ef
Yorgo Bou Samra <71931530+YorgoBS@users.noreply.github.com>
2022-10-10 16:38:03 +0300

    Final Bug Fixes

---
+++ project_435L/project_435L/Main.java
@@ -0,0 +1,16 @@
+package project_435L;
+
+import java.util.Map;
+
+public class Main {
+
+	public static void main(String[] args) {
+	Calculator calc = new Calculator(3);
+	calc.generateExpressionList(50);
+		Map<String,Integer> testMap = calc.getExpMap();
+		for (Map.Entry<String,Integer> entry : testMap.entrySet())  
+            System.out.println("Key = " + entry.getKey() + 
+                             ", Value = " + entry.getValue()); 
+    } 
+}
+
